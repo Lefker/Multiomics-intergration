e673edebf4b67f25a7dc758f491649803e7464fc
Lefker <106658456+Lefker@users.noreply.github.com>
2024-07-21 21:01:35 +0300

    Update 5_MiRnaPRDAMachineLearning.R

--- 5_MiRnaPRDAMachineLearning.R
+++ 5_MiRnaPRDAMachineLearning.R
@@ -4,53 +4,50 @@ library(rpart)
 library(rpart.plot)
 library(smotefamily)
 # Acquires the data
-FinalDataCond_miRNA <- read.delim("file_path_significantmiRNA.tab", row.names=1)
+FinalDataCond_miRNA <- read.delim("K:\\METAPTYXIAKO\\Diplomatikh\\Ergasia\\significantmiRNA.tab", row.names=1)
 
 
-# Creates the training and testing set
-inTrain <- createDataPartition(FinalDataCond_miRNA$Condition, p = 0.75, list = FALSE, times = 1)
-training<-FinalDataCond_miRNA[inTrain,]
-testing <-FinalDataCond_miRNA[-inTrain,]
 # Prepares the model's parametres
 set.seed(50)
-
-train.smote <- SMOTE(training[,-1],training$Condition,K = 5)
-train.smote <- train.smote$data # extract only the balanced dataset
-train.smote$class <- as.factor(train.smote$class)
-prop.table(table(train.smote$class))
+Smote_data_miRNA <- SMOTE(FinalDataCond_miRNA[,-1],FinalDataCond_miRNA$Condition,K = 5)
+Smote_data_miRNA <- Smote_data_miRNA$data # extract only the balanced dataset
+Smote_data_miRNA$class <- as.factor(Smote_data_miRNA$class)
+inTrain <- createDataPartition(Smote_data_miRNA$class, p = 0.75, list = FALSE, times = 1)
+training_miRNA<-Smote_data_miRNA[inTrain,]
+testing_miRNA <-Smote_data_miRNA[-inTrain,]
 
 control <-trainControl(method='cv', number = 10)
 metric <- "Accuracy"
 # Trains the three models and gets the confusion matrices with the classification types
 set.seed(50)
-fit.knn<-train(class~., data=train.smote, method="knn", metric=metric, trControl=control)
-predknn<-predict(fit.knn,testing)
-confusionMatrix(predknn,as.factor(testing$Condition))
-knnRaw<-predict(fit.knn, newdata = testing, type = "raw")
+fit.knn<-train(class~., data=training_miRNA, method="knn", metric=metric, trControl=control)
+predknn<-predict(fit.knn,testing_miRNA)
+confusionMatrix(predknn,as.factor(testing_miRNA$class))
+knnRaw<-predict(fit.knn, newdata = testing_miRNA, type = "raw")
 knnRaw<-as.data.frame(knnRaw)
 colnames(knnRaw)<- c("Classification Condition")
-knnR<-cbind("Original Condition"=testing$Condition, knnRaw)
-row.names(knnR)<-rownames(testing)
+knnR<-cbind("Original Condition"=testing_miRNA$class, knnRaw)
+row.names(knnR)<-rownames(testing_miRNA)
 
 set.seed(50)
-fit.svm<-train(class ~., data=train.smote, method="svmRadial", metric=metric, trControl=control, tuneLength = 10)
-predsvm<-predict(fit.svm,testing)
-confusionMatrix(predsvm,as.factor(testing$Condition))
-svmRaw<-predict(fit.svm, newdata = testing, type = "raw")
+fit.svm<-train(class ~., data=training_miRNA, method="svmRadial", metric=metric, trControl=control, tuneLength = 10)
+predsvm<-predict(fit.svm,testing_miRNA)
+confusionMatrix(predsvm,as.factor(testing_miRNA$class))
+svmRaw<-predict(fit.svm, newdata = testing_miRNA, type = "raw")
 svmRaw<-as.data.frame(svmRaw)
 colnames(svmRaw)<- c("Classification Condition")
-svmR<-cbind("Original Condition"=testing$Condition, svmRaw)
-row.names(svmR)<-rownames(testing)
+svmR<-cbind("Original Condition"=testing_miRNA$class, svmRaw)
+row.names(svmR)<-rownames(testing_miRNA)
 
 set.seed(50)
-fit.rpart<-train(class~., data=train.smote, method="rpart", metric=metric, trControl=control)
-predrpart<-predict(fit.rpart,testing)
-confusionMatrix(predrpart,as.factor(testing$Condition))
-rpartRaw<-predict(fit.rpart, newdata = testing, type = "raw")
+fit.rpart<-train(class~., data=training_miRNA, method="rpart", metric=metric, trControl=control)
+predrpart<-predict(fit.rpart,testing_miRNA)
+confusionMatrix(predrpart,as.factor(testing_miRNA$class))
+rpartRaw<-predict(fit.rpart, newdata = testing_miRNA, type = "raw")
 rpartRaw<-as.data.frame(rpartRaw)
 colnames(rpartRaw)<- c("Classification Condition")
-rpartR<-cbind("Original Condition"=testing$Condition, rpartRaw)
-row.names(rpartR)<-rownames(testing)
+rpartR<-cbind("Original Condition"=testing_miRNA$class, rpartRaw)
+row.names(rpartR)<-rownames(testing_miRNA)
 rpart.plot(fit.rpart$finalModel)
 
 # Gets the results of the training models
